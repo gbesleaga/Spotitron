d4ac38d5038cc13c45baefe503d1c3aba3e3ff1c
AnotherNoobDev <gebesleaga@gmail.com>
2021-03-23 20:20:19 +0100

    setup spotify login

--- spotitron/src/app/app.module.ts
+++ spotitron/src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthRedirectComponent } from './auth/auth-redirect/auth-redirect.component';
+import { AuthCallbackComponent } from './auth/auth-callback/auth-callback.component';
+import { RouterModule, Routes } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { AuthGuardService, AuthService, SpotifyHttpClientService, SpotifyLibModule } from 'spotify-lib';
+import { HttpClientModule } from '@angular/common/http';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: AuthRedirectComponent
+  },
+  { 
+    path: environment.spotifyCallBackRoute, 
+    component: AuthCallbackComponent 
+  },
+  { 
+    path: '**', 
+    redirectTo: ''
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthRedirectComponent,
+    AuthCallbackComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    SpotifyLibModule.forRoot({
+      redirectURI: environment.spotifyAuthRedirectURI,
+      clientId: '40bdabd3ffb14520bebc8e90e97c0c77',
+      clientScope: 'playlist-read-private streaming user-read-email user-read-private user-top-read playlist-modify-public',
+      storageKeyForCodeVerifier: 'STRON_codeVerifier',
+      storageKeyForAuthToken: 'STRON_authToken',
+      storageKeyForAuthTokenValidUntil: 'STRON_authTokenValidUntil',
+      storageKeyForAuthRequestState: 'STRON_state'
+    })
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuardService,
+    SpotifyHttpClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
