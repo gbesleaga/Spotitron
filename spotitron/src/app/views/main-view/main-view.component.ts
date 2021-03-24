76f44b89767287eda08f7f2d0c914dd186f8f2eb
AnotherNoobDev <gebesleaga@gmail.com>
2021-03-24 20:32:53 +0100

    retrieve charts for all countries

--- spotitron/src/app/views/main-view/main-view.component.ts
+++ spotitron/src/app/views/main-view/main-view.component.ts
@@ -1,8 +1,11 @@
 import { AfterViewInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { forkJoin, of } from 'rxjs';
 import { AuthService, SpotifyHttpClientService, SpotifyPlaylistTrackObject } from 'spotify-lib';
 import { AnimationService } from 'src/app/rendering/animation.service';
 import { RenderingService } from 'src/app/rendering/rendering.service';
+import { CountryDataService } from 'src/app/shared/country-data.service';
+import { catchError, map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-main-view',
@@ -11,25 +14,60 @@ import { RenderingService } from 'src/app/rendering/rendering.service';
 })
 export class MainViewComponent implements AfterViewInit {
 
-  constructor(private renderingService: RenderingService,
+  private charts: any[] = [];
+
+  constructor(
+    private countryDataService: CountryDataService,
+    private renderingService: RenderingService,
     private animationService: AnimationService,
     private authService: AuthService,
     private spotifyService: SpotifyHttpClientService ) { }
 
   ngAfterViewInit(){
-    this.spotifyService.getCountryChart({accessToken: this.authService.getAccessToken(), countryName: 'Jifu' }).subscribe( 
-      chart => {
-        const playlistItems = chart.tracks.items as SpotifyPlaylistTrackObject[];
+    this.fetchNextCountry(0, this.countryDataService.countryNames.length);
+  }
 
-        for (let item of playlistItems) {
-          console.log(item.track.album.images);
+  private fetchNextCountry(at: number, stop: number) {
+    if (at >= stop) {
+      this.readyToRender();
+      return;
+    }
+
+    const requests = [];
+
+    let step = stop - at;
+
+    if (step > 30) step = 30;
+
+    for (let i = at; i < at + step; ++i) {
+      const request =  this.spotifyService.getCountryChart({accessToken: this.authService.getAccessToken(), countryName: this.countryDataService.countryNames[i] }).pipe(catchError(error => of(error)), map(chart => ({...chart, country: this.countryDataService.countryNames[i]})));
+      requests.push(request);
+    }
+
+   forkJoin(requests).subscribe( 
+    responseList => {
+      for (let chart of responseList) {
+        if (chart && chart.tracks) {
+          const playlistItems = chart.tracks.items as SpotifyPlaylistTrackObject[];
+
+          if (playlistItems) {
+            this.charts.push(chart);
+          }
         }
-      },
-      err => {
-        console.log("An error occured");
       }
-    );
-    
+      this.fetchNextCountry(at + step, stop);
+    },
+    err => {
+      console.log("An error occured: " + err);
+    });
+  }
+
+  private readyToRender() {
+    for (let chart of this.charts) {
+      const playlistItems = chart.tracks.items as SpotifyPlaylistTrackObject[];
+      console.log(chart.country + " : " + playlistItems[0].track.name);
+    }
+
     this.renderingService.init();
     this.animationService.animate();
   }
