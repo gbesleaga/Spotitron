ea7fcb59fbb399c193134b58ec0abcc2202e7238
AnotherNoobDev <gebesleaga@gmail.com>
2021-04-20 20:36:44 +0200

    adjust environment for prod

--- spotitron/src/environments/environment.prod.ts
+++ spotitron/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  spotifyAuthRedirectURI: 'https://spotitron.web.app/auth-callback',
+  spotifyCallBackRoute: 'auth-callback'
 };
