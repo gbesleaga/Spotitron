60219ad2bdc11859d71059ae050854e1634a2687
AnotherNoobDev <69325117+AnotherNoobDev@users.noreply.github.com>
2021-11-13 10:08:40 +0100

    Delete environment.ts
    upsy

--- spotitron/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false,
-  spotifyAuthRedirectURI: 'http://localhost:4200/auth-callbackTzwRLzBu66Wi8DrpNUpDn22jB6mpaQp4Oc5eqQny9Ng7DaH6FXy5k90cSXG4o8Hv4m3uOmFxIwghjOflDIgaow',
-  spotifyCallBackRoute: 'auth-callbackTzwRLzBu66Wi8DrpNUpDn22jB6mpaQp4Oc5eqQny9Ng7DaH6FXy5k90cSXG4o8Hv4m3uOmFxIwghjOflDIgaow'
-};
